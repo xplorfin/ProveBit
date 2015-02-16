a3a867faac870a11ad6d063b34966d86c6cc2e04
Daniel Boehm <boehm3@illinois.edu>
2015-02-16 17:31:36 -0600

    cleaning branch

--- ProveBit/src/org/provebit/proof/Proof.java
+++
@@ -1,5 +0,0 @@
-package org.provebit.proof;
-
-public class Proof {
-
-}
--- ProveBit/src/org/provebit/wallet/ProveBitWallet.java
+++
@@ -1,9 +0,0 @@
-package org.provebit.wallet;
-
-public class ProveBitWallet {
-
-	public ProveBitWallet() {
-		// TODO Auto-generated constructor stub
-	}
-
-}

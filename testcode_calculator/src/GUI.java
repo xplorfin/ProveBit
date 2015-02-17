27b70178e0c2f2ac3490b368489a53469a0fcafa
ethanding555 <ethanding555@gmail.com>
2015-02-16 19:10:52 -0600

    started working on MigLayout

--- testcode_calculator/src/GUI.java
+++ testcode_calculator/src/GUI.java
@@ -1,11 +1,39 @@
-/**
- * 
- */
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
-/**
- * @author Shishirkanodia
- *
- */
+import net.miginfocom.swing.MigLayout;
+
 public class GUI {
+	JFrame frame = new JFrame("Calculator");
+	JPanel panel = new JPanel();
+
+	JButton btnOne = new JButton("1");
+	JButton btnTwo = new JButton("2");
+
+	public GUI() {
+		panel.setLayout(new MigLayout());
+		panel.add(btnOne);
+		panel.add(btnTwo);
+		
+		frame.add(panel);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new GUI();
+			}
+		});
+	}
 
 }

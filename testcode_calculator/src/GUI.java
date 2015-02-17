d5324627fbfd0b04918b283bc873e8dfe90ee32a
ethanding555 <ethanding555@gmail.com>
2015-02-17 17:27:15 -0600

    all numeric buttons have actionListeners and actions

--- testcode_calculator/src/GUI.java
+++ testcode_calculator/src/GUI.java
@@ -37,22 +37,34 @@ public class GUI implements ActionListener{
 	public GUI() {
 		panel.setLayout(new MigLayout());
 		// display screen for the calculator
-		panel.add(screen, "wrap");
-		// numeric buttons
+		panel.add(screen, "span");
+		// numeric and operation buttons
 		panel.add(btnOne);
 		btnOne.addActionListener(this);
 		panel.add(btnTwo);
+		btnTwo.addActionListener(this);
 		panel.add(btnThree);
+		btnThree.addActionListener(this);
 		panel.add(btnAdd, "wrap");
+		btnAdd.addActionListener(this);
 		panel.add(btnFour);
+		btnFour.addActionListener(this);
 		panel.add(btnFive);
+		btnFive.addActionListener(this);
 		panel.add(btnSix);
+		btnSix.addActionListener(this);
 		panel.add(btnSub, "wrap");
+		btnSub.addActionListener(this);
 		panel.add(btnSeven);
+		btnSeven.addActionListener(this);
 		panel.add(btnEight);
+		btnEight.addActionListener(this);
 		panel.add(btnNine);
+		btnNine.addActionListener(this);
 		panel.add(btnEqn, "wrap");
+		btnEqn.addActionListener(this);
 		panel.add(btnZero);
+		btnZero.addActionListener(this);
 		
 		frame.add(panel);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -62,20 +74,55 @@ public class GUI implements ActionListener{
 	
 	public void actionPerformed(ActionEvent e){
 		switch (e.getActionCommand()){
-		case "1": appendNumToOutputString(1);
-		break;
+			case "1":
+				appendNumToOutputString(1);
+				break;
+			case "2": 
+				appendNumToOutputString(2);
+				break;
+			case "3": 
+				appendNumToOutputString(3);
+				break;
+			case "4": 
+				appendNumToOutputString(4);
+				break;
+			case "5": 
+				appendNumToOutputString(5);
+				break;
+			case "6": 
+				appendNumToOutputString(6);
+				break;
+			case "7": 
+				appendNumToOutputString(7);
+				break;
+			case "8": 
+				appendNumToOutputString(8);
+				break;
+			case "9": 
+				appendNumToOutputString(9);
+				break;
+			case "0": 
+				appendNumToOutputString(0);
+				break;
 		}
 	}
 
 	public void appendNumToOutputString(int inputNum) {
 		StringBuilder outputStringBuilder = new StringBuilder();
-		outputStringBuilder.append(screen.getText());
-		outputStringBuilder.append(inputNum);
-		screen.setText(outputStringBuilder.toString());
+		// if screen only has "0" on it
+		if(screen.getText().equals("0")) {
+			outputStringBuilder.append(inputNum);
+			screen.setText(outputStringBuilder.toString());
+		}
+		// normal case
+		else {
+			outputStringBuilder.append(screen.getText());
+			outputStringBuilder.append(inputNum);
+			screen.setText(outputStringBuilder.toString());
+		}
 	}
 	
 	public static void main(String[] args) {
-		
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {

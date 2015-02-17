05d9138ca5173e3a302be328b83838b45cc6962c
Daniel Boehm <boehm3@illinois.edu>
2015-02-16 18:02:28 -0600

    fixed completedir path

--- ProveBit/tests/org/provebit/merkle/tests/merkleTests.java
+++ ProveBit/tests/org/provebit/merkle/tests/merkleTests.java
@@ -8,62 +8,64 @@ import org.junit.Test;
 import org.provebit.merkle.Merkle;
 
 public class merkleTests {
-	static String COMMONSPATH = "/commons-io-2.4";
-	static String DOCSPATH = COMMONSPATH + "/docs";
-	static String commonsDirPath;
-	static String docsDirPath;
+	static String COMPLETEDIR = "/tests/org/provebit/merkle/tests/testCompleteDir";
+	static String INCOMPLETEDIR = "/tests/org/provebit/merkle/tests/testIncompleteDir";
+	static String completeDirPath;
+	static String incompleteDirPath;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		commonsDirPath = new java.io.File( "." ).getCanonicalPath() + COMMONSPATH;
-		docsDirPath = new java.io.File( "." ).getCanonicalPath() + DOCSPATH;
+		completeDirPath = new java.io.File( "." ).getCanonicalPath() + COMPLETEDIR;
+		incompleteDirPath = new java.io.File( "." ).getCanonicalPath() + INCOMPLETEDIR;
+		System.out.println(completeDirPath);
+		System.out.println(incompleteDirPath);
 	}
 
 	@Test
 	public void testTreeHeightEvenLeaves() {
-		Merkle mTree = new Merkle(commonsDirPath);
+		Merkle mTree = new Merkle(completeDirPath);
 		mTree.makeTree();
 		assertTrue(mTree.getHeight() == 3);
 	}
 	
 	@Test
 	public void testTreeHeightOddLeaves() {
-		Merkle mTree = new Merkle(docsDirPath);
+		Merkle mTree = new Merkle(incompleteDirPath);
 		mTree.makeTree();
 		assertTrue(mTree.getHeight() == 4);
 	}
 
 	@Test
 	public void testNumLeavesEven() {
-		Merkle mTree = new Merkle(commonsDirPath);
+		Merkle mTree = new Merkle(completeDirPath);
 		mTree.makeTree();
 		assertTrue(mTree.getNumLeaves() == 8);
 	}
 	
 	@Test
 	public void testNumLeavesOdd() {
-		Merkle mTree = new Merkle(docsDirPath);
+		Merkle mTree = new Merkle(incompleteDirPath);
 		mTree.makeTree();
 		assertTrue(mTree.getNumLeaves() == 14);
 	}
 	
 	@Test
 	public void testTreeSizeEven() {
-		Merkle mTree = new Merkle(commonsDirPath);
+		Merkle mTree = new Merkle(completeDirPath);
 		mTree.makeTree();
 		assertTrue(mTree.getTreeSize() == 15);
 	}
 	
 	@Test
 	public void testTreeSizeOdd() {
-		Merkle mTree = new Merkle(docsDirPath);
+		Merkle mTree = new Merkle(incompleteDirPath);
 		mTree.makeTree();
 		assertTrue(mTree.getTreeSize() == 28);
 	}
 	
 	@Test
 	public void testLeafPositions() {
-		Merkle mTree = new Merkle(commonsDirPath);
+		Merkle mTree = new Merkle(completeDirPath);
 		mTree.makeTree();
 		byte[][] tree = mTree.getTree();
 		int i = (int) Math.pow(2, mTree.getHeight()) - 1;

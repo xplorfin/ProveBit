1b49b11a1ef16f3ec6073368354017611c48036e
ethanding555 <ethanding555@gmail.com>
2015-02-16 20:01:43 -0600

    "Basic Layout for the Calculator - qding5 & skanodi2"

--- testcode_calculator/src/GUI.java
+++ testcode_calculator/src/GUI.java
@@ -1,3 +1,6 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -12,14 +15,49 @@ import net.miginfocom.swing.MigLayout;
 public class GUI {
 	JFrame frame = new JFrame("Calculator");
 	JPanel panel = new JPanel();
-
+	JLabel screen = new JLabel("0");
+	
 	JButton btnOne = new JButton("1");
 	JButton btnTwo = new JButton("2");
+	JButton btnThree = new JButton("3");
+	JButton btnFour = new JButton("4");
+	JButton btnFive = new JButton("5");
+	JButton btnSix = new JButton("6");
+	JButton btnSeven = new JButton("7");
+	JButton btnEight = new JButton("8");
+	JButton btnNine = new JButton("9");
+	JButton btnZero = new JButton("0");
+	
+	JButton btnAdd = new JButton("+");
+	JButton btnSub = new JButton("-");
+	JButton btnMul = new JButton("*");
+	JButton btnDiv = new JButton("/");
+	JButton btnEqn = new JButton("=");
+	
+	
 
 	public GUI() {
+		btnOne.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				screen.setText("1");
+			}
+		});
 		panel.setLayout(new MigLayout());
+		panel.add(screen, "wrap");
 		panel.add(btnOne);
 		panel.add(btnTwo);
+		panel.add(btnThree);
+		panel.add(btnAdd, "wrap");
+		panel.add(btnFour);
+		panel.add(btnFive);
+		panel.add(btnSix);
+		panel.add(btnSub, "wrap");
+		panel.add(btnSeven);
+		panel.add(btnEight);
+		panel.add(btnNine);
+		panel.add(btnEqn, "wrap");
+		panel.add(btnZero);
 		
 		frame.add(panel);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -28,6 +66,7 @@ public class GUI {
 	}
 
 	public static void main(String[] args) {
+		
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {

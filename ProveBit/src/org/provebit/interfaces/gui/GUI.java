182f9329e6950f102b716548e405cbad77aad7b3
ethanding555 <ethanding555@gmail.com>
2015-02-18 04:13:27 -0600

    Basic implementation of layout

--- ProveBit/src/org/provebit/interfaces/gui/GUI.java
+++ ProveBit/src/org/provebit/interfaces/gui/GUI.java
@@ -1,11 +1,43 @@
 package org.provebit.interfaces.gui;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class GUI {
+import net.miginfocom.swing.MigLayout;
 
+public class GUI {
+	JFrame frame = new JFrame("ProveBit");
+	JMenuBar menuBar = new JMenuBar();
+	JMenu menuAbout = new JMenu("About");
+	JMenuItem menuItemAboutUs = new JMenuItem("About Us");
+	JPanel panelMain = new JPanel();
+	JLabel labelTitle = new JLabel("ProveBit");
+	JButton btnProve = new JButton("Prove a File");
+	JButton btnVerify = new JButton("Verify a Certificate");
+	JButton btnWallet = new JButton("Bitcoin Wallet");
+	
 	public GUI() {
-		
+		// Set up menu bar and menu
+		menuAbout.add(menuItemAboutUs);
+		menuBar.add(menuAbout);
+		frame.setJMenuBar(menuBar);
+		// Set up main panel layout
+		panelMain.setLayout(new MigLayout());
+		panelMain.add(btnProve);
+		panelMain.add(btnVerify);
+		panelMain.add(btnWallet);
+		panelMain.add(labelTitle, "dock north, gapleft 150");
+		// Pack up frame
+		frame.add(panelMain);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
 	}
 	
 	public static void main(String[] args) {

568008e38b230357c85550e2d382a9bbaa5caa0d
Steve Halm <thereal1024@gmail.com>
2015-02-23 18:43:12 -0600

    remove old project

--- ProveBit-old/src/org/provebit/proof/keysys/AbstractKeyNode.java
+++
@@ -1,9 +0,0 @@
-package org.provebit.proof.keysys;
-
-import java.util.Arrays;
-
-public abstract class AbstractKeyNode {
-	
-	protected abstract byte[] keyLookupRecurse(String[] vals, int i);
-
-}
--- ProveBit-old/src/org/provebit/ui/ProveBit.java
+++
@@ -1,5 +0,0 @@
-package org.provebit.ui;
-
-public class ProveBit {
-
-}

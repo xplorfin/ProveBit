282d6d9a7dfcb34c17bd4bc7317aa7e9ce87ca23
ethanding555 <ethanding555@gmail.com>
2015-03-04 10:37:51 -0600

    Added tab in main frame

--- provebit-parent/provebit-core/src/main/java/org/provebit/ui/MainFrame.java
+++ provebit-parent/provebit-core/src/main/java/org/provebit/ui/MainFrame.java
@@ -1,11 +1,10 @@
 package org.provebit.ui;
 
-import java.awt.Menu;
-
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JTabbedPane;
 import javax.swing.WindowConstants;
 
 import net.miginfocom.swing.MigLayout;
@@ -18,18 +17,34 @@ public class MainFrame extends JFrame {
 	public JMenu menuAbout = new JMenu("About");
 	public JMenuItem menuItemAboutUs = new JMenuItem("About Us");
 	
+	public DaemonConfigTab daemonConfigTab = new DaemonConfigTab();
+	
 	public MainFrame() {
 		setLayout(new MigLayout());
 		// set up menu bar
-		menuFile.add(menuItemFileQuit);
-		menuAbout.add(menuItemAboutUs);
-		menuBar.add(menuFile);
-		menuBar.add(menuAbout);
-		this.setJMenuBar(menuBar);
+		addMenuBar();
+		
+		// set up tabbed pane
+		addTabs();
 		
 		// pack up
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         pack();
         setVisible(true);
+	}
+	
+	public void addMenuBar() {
+		menuFile.add(menuItemFileQuit);
+		menuAbout.add(menuItemAboutUs);
+		menuBar.add(menuFile);
+		menuBar.add(menuAbout);
+		this.setJMenuBar(menuBar);
+	}
+	
+	public void addTabs() {
+		JTabbedPane tabbedPane = new JTabbedPane();
+		tabbedPane.addTab("Daemon Config", daemonConfigTab);
+		tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+		this.add(tabbedPane);
 	}
 }

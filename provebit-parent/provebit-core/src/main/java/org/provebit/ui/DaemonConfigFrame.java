0b1904b44318a18822aabd81000daa29891beda5
Matthew Knox <mpknox2@illinois.edu>
2015-03-03 23:13:30 -0600

    Added skeleton class for the Deamon Configuration subwindow.

---
+++ provebit-parent/provebit-core/src/main/java/org/provebit/ui/DaemonConfigFrame.java
@@ -0,0 +1,43 @@
+package org.provebit.ui;
+/**
+ * 
+ */
+
+/** Swing Imports  */
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+
+public class DaemonConfigFrame extends JFrame {
+	
+	
+	private static final String ADDFILESTRING = "Add Files to Monitor...";
+	private static final String REMOVEFILESTRING = "Remove Files...";
+	
+	private JButton addFileButton;
+	private JButton removeFileButton;
+	
+	private JList fileList;
+	
+	private JTextField runPeriodInput;
+	
+	public DaemonConfigFrame() {
+		super();
+		
+		addFileButton = new JButton(ADDFILESTRING);
+		addFileButton.setEnabled(true);
+		
+		removeFileButton = new JButton(REMOVEFILESTRING);
+		removeFileButton.setEnabled(true);
+		
+		fileList = new JList();
+		fileList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		fileList.setLayoutOrientation(JList.VERTICAL);
+		fileList.setVisibleRowCount(7);
+		
+		runPeriodInput = new JTextField();
+		
+	}
+}

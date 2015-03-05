46574122a57bbd92b0d3ea7c1a1115593b27bbd7
Daniel Boehm <boehm3@illinois.edu>
2015-03-04 20:16:24 -0600

    Add comments to advancedView for guide

--- provebit-parent/provebit-core/src/main/java/org/provebit/ui/advanced/AdvancedView.java
+++ provebit-parent/provebit-core/src/main/java/org/provebit/ui/advanced/AdvancedView.java
@@ -7,19 +7,27 @@ import java.util.Observer;
 import javax.swing.JPanel;
 
 public class AdvancedView extends JPanel implements Observer {
-
+	AdvancedModel model;
+	
 	public AdvancedView(AdvancedModel model) {
+		this.model = model;
 		
+		// Put view controls here
+		// Buttons, labels, textboxes, lists, etc...
+		
+		setVisible(true);
 	}
 	
 	public void addController(ActionListener controller) {
-		
+		// Iterate over controls that require an action listener
+		// and add 'controller' => object.addActionListener(controller)
 	}
 	
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
-		
+		// If there is model implementation (not expected at this point)
+		// This callback will be notified of model changes and should
+		// appropriately update the view
 	}
 
 }

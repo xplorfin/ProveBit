37e0ce620ada3f12a9d879f366b7b7b32cc17cac
Steve Halm <thereal1024@gmail.com>
2015-04-14 01:54:31 -0500

    add proof for file in GUI

--- provebit-parent/provebit-core/src/main/java/org/provebit/ui/general/GeneralModel.java
+++ provebit-parent/provebit-core/src/main/java/org/provebit/ui/general/GeneralModel.java
@@ -5,13 +5,21 @@ import java.util.Observable;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.*;
 
+import org.bitcoinj.core.InsufficientMoneyException;
+import org.bitcoinj.core.Transaction;
+import org.provebit.systems.bitcoin.wallet.ApplicationWallet;
+
 public class GeneralModel extends Observable {
 	
+	private final static ApplicationWallet appwallet = ApplicationWallet.INSTANCE;
 	
 	public GeneralModel() {
 		// TODO
 	}
 	
+	public Transaction proofTX(byte[] hash) throws InsufficientMoneyException {
+		return appwallet.proofTX(hash);
+	}
 	
 	private void notifyChange() {
 		setChanged();

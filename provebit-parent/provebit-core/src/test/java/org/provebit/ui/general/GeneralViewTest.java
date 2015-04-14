20b9c0c5bfb11cf4716b3b2f4f48fe06b1f1dba1
ethanding555 <ethanding555@gmail.com>
2015-04-14 05:29:39 -0500

    add some more test cases and comments in GeneralViewTest and MainViewTest

--- provebit-parent/provebit-core/src/test/java/org/provebit/ui/general/GeneralViewTest.java
+++ provebit-parent/provebit-core/src/test/java/org/provebit/ui/general/GeneralViewTest.java
@@ -13,16 +13,26 @@ public class GeneralViewTest extends UISpecTestCase {
 	private Window window;
 	private Panel generalPane;
 	
+	/**
+	 * this function call super.setUp() for some initial setup,
+	 * and then set the adaptation between test case and main class
+	 * to intercept the main window of the application
+	 */
 	public void setUp() throws Exception {
 		super.setUp();
 		UISpec4J.setWindowInterceptionTimeLimit(10000);
 		setAdapter(new MainClassAdapter(RunGUI.class, new String[0]));
 		window = getMainWindow();
+		
+		// retrieve general panel from the main window
 		TabGroup tabGroup = window.getTabGroup("Main");
 		tabGroup.selectTab("General");
 		generalPane = tabGroup.getSelectedTab();
 	}
 	
+	/**
+	 * test the header name of the table
+	 */
 	public void testTableHeader() {
 		assertTrue(generalPane.getTable().getHeader().contentEquals(new String[] {
 				"File", "Status"
--- provebit-parent/provebit-core/src/test/java/org/provebit/ui/main/MainViewTest.java
+++ provebit-parent/provebit-core/src/test/java/org/provebit/ui/main/MainViewTest.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.*;
 
 import org.provebit.ui.RunGUI;
 import org.uispec4j.Button;
+import org.uispec4j.Panel;
+import org.uispec4j.TabGroup;
 import org.uispec4j.Trigger;
 import org.uispec4j.UISpec4J;
 import org.uispec4j.UISpecTestCase;
@@ -16,8 +18,15 @@ import org.uispec4j.interception.WindowHandler;
 import org.uispec4j.interception.WindowInterceptor;
 
 public class MainViewTest extends UISpecTestCase {
-	private Window window;
 	
+	private Window window;
+	private TabGroup tabGroup;
+	
+	/**
+	 * this function call super.setUp() for some initial setup,
+	 * and then set the adaptation between test case and main class
+	 * to intercept the main window of the application
+	 */
 	public void setUp() throws Exception {
 		super.setUp();
 		UISpec4J.setWindowInterceptionTimeLimit(10000);
@@ -25,16 +34,61 @@ public class MainViewTest extends UISpecTestCase {
 		window = getMainWindow();
 	}
 	
+	/**
+	 * test the menu bar options
+	 */
+	public void testMenuBarContent() {
+		assertTrue(window.getMenuBar().contentEquals(new String[]{"File", "About"}));
+	}
 	
+	/**
+	 * invoke the About Us dialog by using trigger to click the button
+	 * on the menu, then call WindowInterceptor for intercepting pop-up
+	 * dialog
+	 * 
+	 * need to close the close this pop-up dialog by return the close
+	 * trigger in .process
+	 */
 	public void testAboutUs() {	
 		WindowInterceptor
+		// set up the trigger to invoke the pop-up dialog
 		.init(window.getMenuBar().getMenu("About").getSubMenu("About Us").triggerClick())
 		.process(new WindowHandler("Test About Us Dialog") {
 			public Trigger process(Window window) throws Exception {
 				assertEquals("About Us", window.getTitle());
+				// return close trigger
 				return window.getButton("OK").triggerClick();
 			}
 		})
 		.run();
 	}
+	
+	/**
+	 * test all the tab name in main TabbedPane
+	 */
+	public void testAllTabNameInTabGroup() {
+		// get a new TabGroup for every test case
+		tabGroup = window.getTabGroup("Main");
+		// this is the expected names for tabs
+		assertTrue(tabGroup.tabNamesEquals(new String[]{"General", "Wallet", "Daemon", "Advanced"}));
+	}
+	
+	/**
+	 * test default tab when the application starts up
+	 */
+	public void testDefaultTabIsGeneral() {
+		tabGroup = window.getTabGroup("Main");
+		// default tab is General
+		assertTrue(tabGroup.selectedTabEquals("General"));
+		// not true
+		assertFalse(tabGroup.selectedTabEquals("Wallet"));
+	}
+	
+	/**
+	 * test every panel in TabbedPane is initialized
+	 */
+	public void testTabPanelIsInitilized() {
+		// TODO test panels are initialized
+	}
+	
 }

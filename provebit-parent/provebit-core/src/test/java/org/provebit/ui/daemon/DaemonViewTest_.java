e1c452fdfca94b88db8d6ec18d0138306e58e314
ethanding555 <ethanding555@gmail.com>
2015-04-24 16:36:20 -0500

    comment out file chooser test case

--- provebit-parent/provebit-core/src/test/java/org/provebit/ui/daemon/DaemonViewTest_.java
+++ provebit-parent/provebit-core/src/test/java/org/provebit/ui/daemon/DaemonViewTest_.java
@@ -1,6 +1,19 @@
 package org.provebit.ui.daemon;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.provebit.ui.RunGUI;
+import org.uispec4j.ListBox;
 import org.uispec4j.Panel;
 import org.uispec4j.TabGroup;
 import org.uispec4j.UISpec4J;
@@ -10,18 +23,26 @@ import org.uispec4j.interception.FileChooserHandler;
 import org.uispec4j.interception.MainClassAdapter;
 import org.uispec4j.interception.WindowInterceptor;
 
+@RunWith(JUnit4.class)
 public class DaemonViewTest_ extends UISpecTestCase {
 	private Window window;
 	private Panel daemonPane;
+	
+	@Rule
+    public TemporaryFolder emptyFolder = new TemporaryFolder();
+	
+	public File emptyDirPath;
+	
 	
 	/**
 	 * this function call super.setUp() for some initial setup,
 	 * and then set the adaptation between test case and main class
 	 * to intercept the main window of the application
 	 */
+	@Before
 	public void setUp() throws Exception {
 		super.setUp();
-		UISpec4J.setWindowInterceptionTimeLimit(100000);
+		UISpec4J.setWindowInterceptionTimeLimit(10000);
 		setAdapter(new MainClassAdapter(RunGUI.class, new String[0]));
 		window = getMainWindow();
 		
@@ -29,18 +50,30 @@ public class DaemonViewTest_ extends UISpecTestCase {
 		TabGroup tabGroup = window.getTabGroup("Main");
 		tabGroup.selectTab("Daemon");
 		daemonPane = tabGroup.getSelectedTab();
+		
+		emptyDirPath = emptyFolder.getRoot();
 	}
 	
 	/**
 	 * invoke and test the file chooser for adding file to daemon
 	 */
-	public void testAddFile() {	
+	@Test
+	public void testAddFile() throws IOException{
+		File tempFile = new File(emptyDirPath.getAbsolutePath() + "/tempFile");
+    	FileUtils.write(tempFile, "temp data");
+    	
 		WindowInterceptor
 		// set up the trigger to invoke the pop-up dialog
 		.init(daemonPane.getButton("Add Files to Monitor...").triggerClick())
 		.process(FileChooserHandler.init()
 				.titleEquals("Open")
-				.cancelSelection())
+				.assertAcceptsFilesAndDirectories()
+//				.select(tempFile.getAbsolutePath()))
+				.select("/home/qding5/test.txt"))
 		.run();
+		
+		ListBox listBox = daemonPane.getListBox();
+//		assertTrue(listBox.contains((tempFile.getAbsolutePath())));
+		assertTrue(listBox.contains("/home/qding5/test.txt"));
 	}
 }
